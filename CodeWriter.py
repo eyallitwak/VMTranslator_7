2cafb1d9b9803740eb281b3c84c792811500edea
Eyal Litwak <eyal.litwak@post.runi.ac.il>
2024-12-14 10:15:18 +0200

    Added static, temp and pointer to CodeWriter

--- CodeWriter.py
+++ CodeWriter.py
@@ -1,3 +1,6 @@
+import os
+
+
 class CodeWriter:
     def __init__(self, dest_file):
         """Creates a CodeWriter object, that will write to the specified file.
@@ -6,10 +9,12 @@ class CodeWriter:
             dest_file (path): Path to create the output file.
         """
         self.file = open(dest_file, 'w')
+        self.file_name = os.path.basename(self.file)
         self.dict = {'local': 'LCL',
                      'argument': 'ARG',
                      'this': 'THIS',
-                     'that': 'THAT'}
+                     'that': 'THAT',
+                     'temp': 5, }
 
     def write_arithmetic(self, command):
         # TODO
@@ -47,12 +52,15 @@ class CodeWriter:
         self.file.write(lines)
 
     def push(self, segment, index):
-        """Writes appropriate ASM commands that implement pushing from local, argument, this or that.
+        """Writes appropriate ASM commands that implement pushing from local, argument, this, that, temp or pointer.
 
         Args:
             segment (str): The segment to push from.
             index (int): The offset index in specified segment.
-        """        
+        """
+        if segment == 'pointer':
+            segment = 'THAT' if index == 1 else 'THIS'
+            index = 0
         lines = '''// D = RAM[{seg} + {i}]
     @{i}
     D=A
@@ -70,12 +78,15 @@ class CodeWriter:
         self.file.write(lines)
 
     def pop(self, segment, index):
-        """Writes appropriate ASM commands that implement popping to local, argument, this or that.
+        """Writes appropriate ASM commands that implement popping to local, argument, this, that, temp or pointer.
 
         Args:
             segment (str): The segment to pop into.
             index (int): The offset index in specified segment.
-        """        
+        """
+        if segment == 'pointer':
+            segment = 'THAT' if index == 1 else 'THIS'
+            index = 0
         lines = '''// RAM[13] = {seg} + {i}
     @{i}
     D=A
@@ -93,3 +104,32 @@ class CodeWriter:
     A=M
     M=D'''.format(seg=self.dict[segment], i=index)
         self.file.write(lines)
+
+    def push_static(self, index):
+        """Implements pushing from static segment.
+
+        Args:
+            index (int): The offset within the segment to push from.
+        """        
+        lines = '''@{label}
+    D=M
+    // RAM[SP] = D
+    @SP
+    A=M
+    M=D
+    // SP++
+    @SP
+    M=M+1'''.format(label=self.file_name.split('.')[0]+'.'+index)
+        self.file.write(lines)
+
+    def pop_static(self, index):
+        """Implement popping into static segment.
+
+        Args:
+            index (int): The offset within the segment to pop into.
+        """        
+        lines = '''M=M-1
+    A=M
+    D=M
+    @{label}
+    M=D'''.format(label=self.file_name.split('.')[0]+'.'+index)

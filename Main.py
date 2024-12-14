3d7f0ef928378f7001c2a6a0fd172927b740b2b4
Eyal Litwak <eyal.litwak@post.runi.ac.il>
2024-12-14 21:10:37 +0200

    Everything works for 1 vm file as input. now to work on directory input

--- Main.py
+++ Main.py
@@ -5,16 +5,21 @@ import os
 
 def main():
     if len(sys.argv) != 2:
-        print("Error: exactly one (1) .vm file or directory as input")
+        print("Error: exactly one (1) .vm file or directory as input expected")
         return
 
     file_name = sys.argv[1]
 
     if os.path.splitext(file_name)[1] != '.vm':
-        print("Error: input file shpuld have the extension \".vm\"")
+        print("Error: input file should have the extension \".vm\"")
         return
 
     if not os.path.isfile(file_name):
         print("Error: given input file doesn't exist")
+        return
 
     VMtranslator.translate(file_name)
+
+
+if __name__ == '__main__':
+    main()

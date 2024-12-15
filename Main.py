a9c8e954e8f5ab788101247dcdf3640046e9dde5
Eyal Litwak <eyal.litwak@post.runi.ac.il>
2024-12-15 20:42:03 +0200

    Groundwork for direcrtory as input

--- Main.py
+++ Main.py
@@ -7,21 +7,33 @@ def main():
     # Terminate if not given exactly 1 input
     if len(sys.argv) != 2:
         print("Error: exactly one (1) .vm file or directory as input expected")
-        return
+        sys.exit()
 
     file_name = sys.argv[1]
 
-    # Terminate if given input is not .vm
-    if os.path.splitext(file_name)[1] != '.vm':
-        print("Error: input file should have the extension \".vm\"")
-        return
+    # When receiving a file as input
+    if os.path.isfile(file_name):
 
-    # Terminate if given input is not an existing file
-    if not os.path.isfile(file_name):
+        # Terminate if given input is not .vm
+        if os.path.splitext(file_name)[1] != '.vm':
+            print("Error: input file should have the extension \".vm\"")
+            sys.exit()
+
+        VMtranslator.translate(file_name)
+
+    # When receiving a directory of .vm files as input
+    elif os.path.isdir(file_name):
+        files = os.listdir(file_name)
+        files = [os.path.join(file_name, f)
+                 for f in files if os.path.splitext(f)[1] == '.vm']
+
+        for f in files:
+            VMtranslator.translate(f)
+
+    # Terminate if given input is not an existing file or directory
+    else:
         print("Error: given input file doesn't exist")
-        return
-
-    VMtranslator.translate(file_name)
+        sys.exit()
 
 
 if __name__ == '__main__':
